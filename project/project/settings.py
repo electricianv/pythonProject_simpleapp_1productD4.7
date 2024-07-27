0aa229496fa68f12eb9bcd7a33353c017577f6fc
Vasil <electricianv@gmail.com>
2024-07-27 18:09:05 +0100

    D3.2

--- project/project/settings.py
+++ project/project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.flatpages',
     'fpages',
+    'simple_app',
 ]
 
 SITE_ID = 1
@@ -118,7 +119,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
